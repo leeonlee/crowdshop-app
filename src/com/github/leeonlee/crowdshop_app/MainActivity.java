a13f9451b16cd01f364d603ce5a2035c5bea2189
Leon Lee <93leonlee@gmail.com>
2014-04-05 06:51:22 -0400

    post request works now

--- src/com/github/leeonlee/crowdshop_app/MainActivity.java
+++ src/com/github/leeonlee/crowdshop_app/MainActivity.java
@@ -1,76 +1,208 @@
 package com.github.leeonlee.crowdshop_app;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
-	
+public class MainActivity extends FragmentActivity implements
+		ActionBar.TabListener {
+
 	private ViewPager viewPager;
 	private TabsPagerAdapter mAdapter;
 	private ActionBar actionBar;
-	private String[] tabs = {"Friends", "Tasks"};
+	private String[] tabs = { "Friends", "Tasks" };
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
- 
-        // Initilization
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        actionBar = getActionBar();
-        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
- 
-        viewPager.setAdapter(mAdapter);
-        actionBar.setHomeButtonEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);        
- 
-        // Adding Tabs
-        for (String tab_name : tabs) {
-            actionBar.addTab(actionBar.newTab().setText(tab_name)
-                    .setTabListener(this));
-        }
-        
-        /**
-         * on swiping the viewpager make respective tab selected
-         * */
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-         
-            @Override
-            public void onPageSelected(int position) {
-                // on changing the page
-                // make respected tab selected
-                actionBar.setSelectedNavigationItem(position);
-            }
-         
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
-            }
-         
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
-            }
-        });
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Initilization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+		new CallPost().execute();
+	}
 
 	@Override
 	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		viewPager.setCurrentItem(tab.getPosition());	
+		viewPager.setCurrentItem(tab.getPosition());
 	}
 
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/*
+	 * Convert JSON string into string
+	 */
+	private static String convertInputStream(InputStream in) throws IOException {
+		int bytesRead;
+		byte[] contents = new byte[1024];
+		String string = null;
+		while ((bytesRead = in.read(contents)) != -1) {
+			string = new String(contents, 0, bytesRead);
+		}
+		return string;
+	}
+
+	private class CallRest extends AsyncTask<String, String, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			String urlString = "http://crowdshop-server.herokuapp.com/tasks";
+
+			String result = "";
+			InputStream in = null;
+			URL url = null;
+			HttpURLConnection urlConnection = null;
+
+			try {
+				url = new URL(urlString);
+
+				urlConnection = (HttpURLConnection) url.openConnection();
+
+				in = new BufferedInputStream(urlConnection.getInputStream());
+
+				result = convertInputStream(in);
+
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				urlConnection.disconnect();
+			}
+			System.out.println(result);
+			return result;
+		}
+
+	}
+
+	private class CallPost extends AsyncTask<String, String, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			String urlString = "http://crowdshop-server.herokuapp.com/loginview/";
+
+			String result = "";
+			InputStream in = null;
+			URL url = null;
+			HttpURLConnection urlConnection = null;
+
+			JSONObject jsonobj; // declared locally so that it destroys after
+								// serving its purpose
+			jsonobj = new JSONObject();
+			try {
+				// adding some keys
+				jsonobj.put("username", "a");
+				jsonobj.put("password", "a");
+
+			} catch (JSONException ex) {
+				ex.printStackTrace();
+			}
+
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppostreq = new HttpPost(urlString);
+			StringEntity se;
+			try {
+				List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+				pairs.add(new BasicNameValuePair("username", "a"));
+				pairs.add(new BasicNameValuePair("password", "a"));
+				httppostreq.setEntity(new UrlEncodedFormEntity(pairs)); 
+				/*
+				se = new StringEntity(jsonobj.toString());
+				se.setContentType("application/json;charset=UTF-8");
+				se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+						"application/json;charset=UTF-8"));
+				httppostreq.setEntity(se);
+				*/
+				HttpResponse httpresponse = httpclient.execute(httppostreq);
+				HttpEntity resultentity = httpresponse.getEntity();
+				InputStream inputstream = resultentity.getContent();
+				String stuff = convertInputStream(inputstream);
+				System.out.println(stuff);
+				
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			System.out.println(result);
+			System.out.println("wtf");
+			return result;
+		}
+
+	}
 }
