85695b0351825a97d12f37a23202370f1364ff41
Leon Lee <93leonlee@gmail.com>
2014-04-05 15:44:22 -0400

    add reward button

--- src/com/github/leeonlee/crowdshop_app/TaskActivity.java
+++ src/com/github/leeonlee/crowdshop_app/TaskActivity.java
@@ -2,12 +2,27 @@ package com.github.leeonlee.crowdshop_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TaskActivity extends Activity{
+	Button submit;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.task_layout);
+		
+		submit = (Button) findViewById(R.id.submit);
+		
+		submit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				
+			}
+		});
 	}
 
 }
