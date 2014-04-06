3b30f41f5e889a9dfeed8f2a8b8f40e71363dff0
Leon Lee <93leonlee@gmail.com>
2014-04-06 06:18:13 -0400

    customize main activity

--- src/com/github/leeonlee/crowdshop_app/TasksAdapter.java
+++ src/com/github/leeonlee/crowdshop_app/TasksAdapter.java
@@ -1,8 +1,8 @@
 package com.github.leeonlee.crowdshop_app;
 
-import java.util.List;
-
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,21 +25,38 @@ public class TasksAdapter extends ArrayAdapter<Long> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater)mApp.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.task_list_item, null);
+		LayoutInflater inflater = (LayoutInflater) mApp
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		RelativeLayout layout = (RelativeLayout) inflater.inflate(
+				R.layout.task_list_item, null);
 		long taskId = getItem(position);
 		TaskInfo taskInfo = mApp.getTaskInfo(taskId);
-		((TextView)layout.findViewById(R.id.task_list_item_title)).setText(taskInfo.name);
-		((TextView)layout.findViewById(R.id.task_list_item_date)).setText(taskInfo.timestamp);
+
+		TextView title = (TextView) layout
+				.findViewById(R.id.task_list_item_title);
+		title.setText(taskInfo.name);
+		title.setTypeface(Typeface.createFromAsset(getContext().getAssets(),
+				"fonts/Hey_Pretty_Girl.ttf"));
+		title.setTextColor(Color.BLACK);
+
+		TextView date = (TextView) layout
+				.findViewById(R.id.task_list_item_date);
+		date.setText(taskInfo.timestamp);
+		date.setTypeface(Typeface.createFromAsset(getContext().getAssets(),
+				"fonts/Hey_Pretty_Girl.ttf"));
+		date.setTextColor(Color.BLACK);
 
 		long thisUserId = mApp.getThisUserId();
-		Long userId = taskInfo.creatorUserId == thisUserId?	taskInfo.claimerUserId : new Long(taskInfo.creatorUserId);
-		if (userId != null)
-		{
+		Long userId = taskInfo.creatorUserId == thisUserId ? taskInfo.claimerUserId
+				: new Long(taskInfo.creatorUserId);
+		if (userId != null) {
 			UserInfo userInfo = mApp.getUserInfo(userId);
-			((TextView)layout.findViewById(R.id.task_list_item_user)).setText(
-				userInfo.firstName + " " + userInfo.lastName
-			);
+			TextView user = (TextView) layout
+					.findViewById(R.id.task_list_item_user);
+			user.setText(userInfo.firstName + " " + userInfo.lastName);
+			user.setTypeface(Typeface.createFromAsset(getContext().getAssets(),
+					"fonts/Hey_Pretty_Girl.ttf"));
+			user.setTextColor(Color.BLACK);
 		}
 		return layout;
 	}
