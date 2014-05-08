f8925c07a0b524f6bc3fa3f32d95715446b6a240
Ten-Young Guh <tenyoung795@gmail.com>
2014-05-07 21:33:33 -0400

    Refactor TaskActivity to use RoboSpice

--- src/com/github/leeonlee/crowdshop_app/TaskActivity.java
+++ src/com/github/leeonlee/crowdshop_app/TaskActivity.java
@@ -1,33 +1,25 @@
 package com.github.leeonlee.crowdshop_app;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.graphics.Typeface;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.github.leeonlee.crowdshop_app.models.Success;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.UrlEncodedContent;
+import com.google.api.client.util.ObjectParser;
+import com.octo.android.robospice.persistence.exception.SpiceException;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TaskActivity extends CrowdShopActivity {
 	Button submit;
@@ -50,7 +42,7 @@ public class TaskActivity extends CrowdShopActivity {
 		title.setTypeface(type);
 		desc = (EditText) findViewById(R.id.desc);
 		desc.setTypeface(type);
-		;
+
 		reward = (EditText) findViewById(R.id.reward);
 		reward.setTypeface(type);
 		budget = (EditText) findViewById(R.id.budget);
@@ -65,96 +57,176 @@ public class TaskActivity extends CrowdShopActivity {
 
 			@Override
 			public void onClick(View arg0) {
-				Context context = getApplicationContext();
-				int duration = Toast.LENGTH_SHORT;
 				String rewardString = reward.getText().toString();
 				String budgetString = budget.getText().toString();
 				String titleString = title.getText().toString();
 				String descString = desc.getText().toString();
 
-				if (titleString.equals("")) {
-					Toast toast = Toast.makeText(context,
-							"Please tell us what you need!", duration);
-					toast.show();
-				} else if (!rewardString.equals("")
-						&& Integer.parseInt(rewardString) < 0) {
-					Toast toast = Toast.makeText(context,
-							"You cannot have a negative reward!", duration);
-					toast.show();
-				} else if (!budgetString.equals("")
-						&& Integer.parseInt(budgetString) < 0) {
-					Toast toast = Toast.makeText(context,
-							"You cannot have a negative budget!", duration);
-					toast.show();
-				} else {
-					new CreateTask().execute(username, titleString, descString,
-							budgetString, rewardString);
+				if (titleString.isEmpty()) {
+					Toast.makeText(TaskActivity.this, R.string.empty_title, Toast.LENGTH_SHORT).show();
+					return;
 				}
+
+				if (budgetString.isEmpty()) {
+					Toast.makeText(TaskActivity.this, R.string.empty_budget, Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				if (rewardString.isEmpty()) {
+					Toast.makeText(TaskActivity.this, R.string.empty_reward, Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				int budgetInt;
+				try {
+					budgetInt = Integer.parseInt(budgetString);
+				} catch (NumberFormatException e) {
+					Toast.makeText(TaskActivity.this, getString(R.string.bad_budget, Integer.MAX_VALUE),
+							Toast.LENGTH_SHORT);
+					return;
+				}
+
+				int rewardInt;
+				try {
+					rewardInt = Integer.parseInt(budgetString);
+				} catch (NumberFormatException e) {
+					Toast.makeText(TaskActivity.this, getString(R.string.bad_reward, Integer.MAX_VALUE),
+							Toast.LENGTH_SHORT);
+					return;
+				}
+
+				CreateTaskFragment.newInstance(username, titleString, descString, budgetInt, rewardInt)
+						.show(getSupportFragmentManager(), "dialog");
 			}
 		});
 	}
 
-	private static String convertInputStream(InputStream in) throws IOException {
-		int bytesRead;
-		byte[] contents = new byte[1024];
-		String string = null;
-		while ((bytesRead = in.read(contents)) != -1) {
-			string = new String(contents, 0, bytesRead);
-		}
-		return string;
-	}
+	private static final class CreateTaskParameters {
 
-	private class CreateTask extends AsyncTask<String, Void, JSONObject> {
-		public void onPreExecute() {
-			pd = new ProgressDialog(TaskActivity.this);
-			pd.setCancelable(true);
-			pd.setMessage("Submitting..");
-			pd.show();
+		public final String username;
+		public final String title;
+		public final String description;
+		public final int budget;
+		public final int reward;
+
+		public CreateTaskParameters(String username, String title, String description, int budget, int reward) {
+			this.username = username;
+			this.title = title;
+			this.description = description;
+			this.budget = budget;
+			this.reward = reward;
 		}
 
 		@Override
-		protected JSONObject doInBackground(String... params) {
-			String urlString = "http://crowdshop-server.herokuapp.com/createtask/";
-			JSONObject result = null;
-			DefaultHttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppostreq = new HttpPost(urlString);
-			try {
-				List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-				pairs.add(new BasicNameValuePair("username", params[0]));
-				pairs.add(new BasicNameValuePair("title", params[1]));
-				pairs.add(new BasicNameValuePair("desc", params[2]));
-				pairs.add(new BasicNameValuePair("threshold", params[3]));
-				pairs.add(new BasicNameValuePair("reward", params[4]));
-				httppostreq.setEntity(new UrlEncodedFormEntity(pairs));
-				HttpResponse httpresponse = httpclient.execute(httppostreq);
-				HttpEntity resultentity = httpresponse.getEntity();
-				InputStream inputstream = resultentity.getContent();
-				String stuff = convertInputStream(inputstream);
-				result = new JSONObject(stuff);
-			} catch (UnsupportedEncodingException e1) {
-				throw new RuntimeException(e1);
-			} catch (ClientProtocolException e) {
-				throw new RuntimeException(e);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			} catch (JSONException e) {
-				throw new RuntimeException(e);
-			}
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
 
+			CreateTaskParameters that = (CreateTaskParameters) o;
+
+			if (budget != that.budget) return false;
+			if (reward != that.reward) return false;
+			if (description != null ? !description.equals(that.description) : that.description != null) return false;
+			if (title != null ? !title.equals(that.title) : that.title != null) return false;
+			if (username != null ? !username.equals(that.username) : that.username != null) return false;
+
+			return true;
+		}
+
+		@Override
+		public int hashCode() {
+			int result = username != null ? username.hashCode() : 0;
+			result = 31 * result + (title != null ? title.hashCode() : 0);
+			result = 31 * result + (description != null ? description.hashCode() : 0);
+			result = 31 * result + budget;
+			result = 31 * result + reward;
 			return result;
 		}
+	}
 
-		protected void onPostExecute(JSONObject result) {
-			pd.cancel();
-			CrowdShopApplication app = (CrowdShopApplication)getApplicationContext();
-			Log.d("", result.toString());
-			int duration = Toast.LENGTH_SHORT;
-			Toast toast = Toast.makeText(app, "Submission complete!",
-					duration);
-			toast.show();
-			finish();
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static final class CreateTaskResult {
+		public Success success;
+	}
+
+	private static final class CreateTaskRequest extends CrowdShopRequest<CreateTaskResult, CreateTaskParameters> {
+
+		private static final String URL = CrowdShopApplication.SERVER + "/createtask";
+
+		public CreateTaskRequest(String username, String title, String description, int budget, int reward) {
+			super(CreateTaskResult.class, new CreateTaskParameters(username, title, description, budget, reward));
 		}
 
+		@Override
+		public CreateTaskResult loadDataFromNetwork() throws Exception {
+			Map<String, Object> body = new HashMap<String, Object>();
+			body.put("username", cacheKey.username);
+			body.put("title", cacheKey.title);
+			body.put("desc", cacheKey.description);
+			body.put("threshold", cacheKey.budget);
+			body.put("reward", cacheKey.reward);
+
+			HttpRequest httpRequest = getHttpRequestFactory().buildPostRequest(
+					new GenericUrl(URL),
+					new UrlEncodedContent(body)
+			);
+			ObjectParser parser = new ObjectMapperParser();
+			httpRequest.setParser(parser);
+			return httpRequest.execute().parseAs(getResultType());
+		}
+	}
+
+	private static final class CreateTaskFragment extends RequestDialogFragment<CreateTaskResult, CreateTaskRequest> {
+
+		private static final String USERNAME = CrowdShopApplication.PACKAGE_NAME + ".USERNAME";
+		private static final String TITLE = CrowdShopApplication.PACKAGE_NAME + ".TITLE";
+		private static final String DESCRIPTION = CrowdShopApplication.PACKAGE_NAME + ".DESCRIPTION";
+		private static final String BUDGET = CrowdShopApplication.PACKAGE_NAME + ".BUDGET";
+		private static final String REWARD = CrowdShopApplication.PACKAGE_NAME + ".REWARD";
+
+		public CreateTaskFragment() {
+			super(CreateTaskResult.class, R.string.submitting);
+		}
+
+		public static CreateTaskFragment newInstance(String username, String title, String description,
+		                                             int budget, int reward) {
+			CreateTaskFragment fragment = new CreateTaskFragment();
+			Bundle args = new Bundle(5);
+			args.putString(USERNAME, username);
+			args.putString(TITLE, title);
+			args.putString(DESCRIPTION, description);
+			args.putInt(BUDGET, budget);
+			args.putInt(REWARD, reward);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		@Override
+		protected CreateTaskRequest newRequest() {
+			Bundle args = getArguments();
+			return new CreateTaskRequest(args.getString(USERNAME), args.getString(TITLE), args.getString(DESCRIPTION),
+					args.getInt(BUDGET), args.getInt(REWARD));
+		}
+
+		@Override
+		protected void onRequestFailure(SpiceException spiceException) {
+			Toast.makeText(getActivity(),
+					getString(R.string.submit_error, spiceException.getLocalizedMessage()),
+					Toast.LENGTH_LONG
+			).show();
+		}
+
+		@Override
+		protected void onRequestSuccess(CreateTaskResult createTaskResult) {
+			if (createTaskResult.success != Success.success) {
+				Toast.makeText(getActivity(), R.string.submit_unknown, Toast.LENGTH_LONG).show();
+			} else {
+				Activity activity = getActivity();
+				Toast.makeText(activity, R.string.submit_ok, Toast.LENGTH_SHORT).show();
+				activity.finish();
+			}
+
+		}
 	}
 
 }
