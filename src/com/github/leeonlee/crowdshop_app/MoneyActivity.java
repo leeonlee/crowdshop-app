9600294630ee5da62c4f9ca862c72bce79b5cbc2
Ten-Young Guh <tenyoung795@gmail.com>
2014-05-10 01:41:45 -0400

    Make MoneyActivity use CrowdShopPostRequest

--- src/com/github/leeonlee/crowdshop_app/MoneyActivity.java
+++ src/com/github/leeonlee/crowdshop_app/MoneyActivity.java
@@ -1,28 +1,18 @@
 package com.github.leeonlee.crowdshop_app;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.graphics.Typeface;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import com.github.leeonlee.crowdshop_app.json.JustSuccess;
+import com.github.leeonlee.crowdshop_app.requests.CrowdShopPostRequest;
+import com.google.api.client.util.Key;
+import com.octo.android.robospice.persistence.exception.SpiceException;
 
 public class MoneyActivity extends CrowdShopActivity {
 	Button submitButton;
@@ -35,6 +25,7 @@ public class MoneyActivity extends CrowdShopActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.money_layout);
 		mTaskId = getIntent().getLongExtra(CrowdShopApplication.TASK_ID, 0);
+		final int threshold = mApp.getTaskInfo(mTaskId).threshold;
 
 		Typeface type = Typeface.createFromAsset(getAssets(),
 				"fonts/Hey_Pretty_Girl.ttf");
@@ -48,63 +39,103 @@ public class MoneyActivity extends CrowdShopActivity {
 			@Override
 			public void onClick(View arg0) {
 				String inputPriceString = inputPrice.getText().toString();
-				if (inputPriceString.equals("")) {
-					Context context = getApplicationContext();
-					int duration = Toast.LENGTH_SHORT;
-					Toast toast = Toast.makeText(context,
-							"Please input number", duration);
-					toast.show();
-				} else {
+				if (inputPriceString.isEmpty())
+					Toast.makeText(MoneyActivity.this,
+							getString(R.string.no_actual_price, threshold), Toast.LENGTH_LONG).show();
+				else {
 					// call some api that marks the price
 					// remember to check for empty values
-					new CreateTask().execute(inputPriceString);
+					int actualPrice = Integer.parseInt(inputPriceString);
+					if (actualPrice > threshold)
+						Toast.makeText(MoneyActivity.this,
+								getString(R.string.bad_actual_price, threshold), Toast.LENGTH_LONG).show();
+					else
+						MyFragment.newInstance(mTaskId, actualPrice)
+							.show(getSupportFragmentManager(), "dialog");
 				}
 			}
 		});
 	}
 
-	private class CreateTask extends AsyncTask<String, Void, Boolean> {
+	public static final class Parameters {
 
-		private int mActualPrice;
-		
-		public void onPreExecute() {
-			pd = new ProgressDialog(MoneyActivity.this);
-			pd.setCancelable(true);
-			pd.setMessage("Submitting..");
-			pd.show();
+		@Key("task_id")
+		public final long taskId;
+		@Key("actual_price")
+		public final int actualPrice;
+
+		public Parameters(long taskId, int actualPrice) {
+			this.taskId = taskId;
+			this.actualPrice = actualPrice;
 		}
 
 		@Override
-		protected Boolean doInBackground(String... params) {
-			String urlString = "http://crowdshop-server.herokuapp.com/confirmpurchase/";
-			DefaultHttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppostreq = new HttpPost(urlString);
-			try {
-				List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-				pairs.add(new BasicNameValuePair("task_id", "" + mTaskId));
-				pairs.add(new BasicNameValuePair("actual_price", params[0]));
-				mActualPrice = Integer.parseInt(params[0]);
-				httppostreq.setEntity(new UrlEncodedFormEntity(pairs));
-				HttpResponse httpresponse = httpclient.execute(httppostreq);
-				return httpresponse.getStatusLine().getStatusCode() == 200;
-			} catch (UnsupportedEncodingException e1) {
-				throw new RuntimeException(e1);
-			} catch (ClientProtocolException e) {
-				throw new RuntimeException(e);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			Parameters that = (Parameters) o;
+
+			if (actualPrice != that.actualPrice) return false;
+			if (taskId != that.taskId) return false;
+
+			return true;
 		}
 
-		protected void onPostExecute(Boolean result) {
-			pd.cancel();
-			Log.d("", result.toString());
-			int duration = Toast.LENGTH_SHORT;
-			Toast toast = Toast
-					.makeText(mApp, "Submission complete!", duration);
-			toast.show();
-			finish();
+		@Override
+		public int hashCode() {
+			int result = (int) (taskId ^ (taskId >>> 32));
+			result = 31 * result + actualPrice;
+			return result;
+		}
+	}
+
+	private static class MyFragment
+			extends RequestDialogFragment<Void, JustSuccess, CrowdShopPostRequest<Parameters, JustSuccess>> {
+
+		public static final String TASK_ID = CrowdShopApplication.PACKAGE_NAME + ".TASK_ID";
+		public static final String ACTUAL_PRICE = CrowdShopApplication.PACKAGE_NAME + ".ACTUAL_PRICE";
+
+		public MyFragment() {
+			super(JustSuccess.class, R.string.submitting);
+		}
+
+		public static MyFragment newInstance(long taskId, int actualPrice) {
+			MyFragment fragment = new MyFragment();
+			Bundle args = new Bundle(2);
+			args.putLong(TASK_ID, taskId);
+			args.putInt(ACTUAL_PRICE, actualPrice);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		@Override
+		protected CrowdShopPostRequest<Parameters, JustSuccess> newRequest() {
+			Bundle args = getArguments();
+			Parameters params = new Parameters(args.getLong(TASK_ID), args.getInt(ACTUAL_PRICE));
+			return CrowdShopPostRequest.make(JustSuccess.class, params, "confirmpurchase");
+		}
+
+		@Override
+		protected void onRequestFailure(SpiceException spiceException) {
+			Toast.makeText(getActivity(),
+					getString(R.string.submit_error, spiceException.getLocalizedMessage()),
+					Toast.LENGTH_LONG
+			).show();
+		}
+
+		@Override
+		protected void onRequestSuccess(Void aVoid) {
+			Activity activity = getActivity();
+			Toast.makeText(activity, R.string.submit_ok, Toast.LENGTH_SHORT).show();
+			activity.finish();
+		}
+
+		@Override
+		protected void onRequestInvalid() {
+			Toast.makeText(getActivity(), R.string.submit_unknown, Toast.LENGTH_LONG).show();
 		}
 
 	}
+
 }
